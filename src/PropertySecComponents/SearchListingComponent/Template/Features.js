072286cfe01f8a516d89322bbe138b4fb637aa1a
OmerWaleed <omerwaleed712@gmail.com>
2018-08-08 16:58:46 +0500

    type and location parent data

--- src/PropertySecComponents/SearchListingComponent/Template/Features.js
+++ src/PropertySecComponents/SearchListingComponent/Template/Features.js
@@ -5,7 +5,6 @@ import SearchListingStyles from "../SearchListingComponent.css";
 class Features extends Component{
 
     render(){
-        console.log(this.props);
         return(
             <div className={SearchListingStyles.listings}>
                 <div className={SearchListingStyles.listingsContainer}>
